96fb8b8445577067ac29279068dd534b05b73582
AuDel-Ada <85498271+AuDel-Ada@users.noreply.github.com>
2021-08-27 12:21:23 +0200

    Add files via upload
    VERSION FINALE AVEC INTEGRATION DE LA MAP

--- src/APIFetch.js
+++ src/APIFetch.js
@@ -1,27 +1,39 @@
+// var idStrAPIFetch = elt.id; // Obtenez l'identifiant
+// elt.id = idStrAPIFetch; // Définir l'identifiant
+// let gen = document.getElementById("btn");
+// gen.addEventListener("click", afficher);
 //const fetch = require("node-fetch");
+
 var ArrayportraitFIP = []
 let arrayPicture = []
 let arrayName = []
+
 const getWomen = async () => {
   const response = await fetch(
     'https://opendata.paris.fr/api/records/1.0/search/?dataset=femmes-illustres-a-paris-portraits&q=&rows=154&sort=short_desc&facet=name&facet=tab_name&facet=short_desc'
   )
+
   if (response.status !== 200) {
     throw new Error('cannot fetch the data')
   }
+
   let i
   const women = await response.json()
   let recordsSplit = women['records']
   for (i = 0; i < recordsSplit.length; i++) {
+
     let resumeParaf = recordsSplit[i]
     let fieldsSplit = resumeParaf['fields']
+
     let name = fieldsSplit['name']
     arrayName.push(name)
+
     let picture = fieldsSplit['thumb_url']
     arrayPicture.push(picture)
+    ​
     let adresse = fieldsSplit['short_desc']
-    let descrip;
 
+    let descrip;
     if(fieldsSplit['desc4']==undefined){
       descrip = `${fieldsSplit['desc1']}<br>${fieldsSplit['desc2']}<br>${fieldsSplit['desc3']}`
     }
@@ -37,27 +49,32 @@ const getWomen = async () => {
     else{
       descrip = `${fieldsSplit['desc1']}<br>${fieldsSplit['desc2']}<br>${fieldsSplit['desc3']}<br>${fieldsSplit['desc4']}<br>${fieldsSplit['desc5']}`
     }
-    let portraitFIP = `Adresse : <br>${adresse}, Paris <br><br> ${descrip}<br>`
+
+    let portraitFIP = `Adresse : ${adresse}, Paris <br> ${descrip}<br>`
+
     let nameAffiche = `${name}`
-   
+
     ArrayportraitFIP.push(portraitFIP)
   }
+
   document.getElementById("petitNom").innerHTML = arrayName[0]
-  console.log(arrayName[0])
-let photo = document.getElementById('photo')
+    let photo = document.getElementById('photo')
 
  photo.setAttribute('src', `${arrayPicture[0]}`)
  document.getElementById("adressAndDesc").innerHTML = ArrayportraitFIP[0]
 }
+
 getWomen()
+
 //fonction pour afficher chaque élément du tableau seul avec un interval de X temps
+
 let compteurAffichage = 1
 function afficher(){
-  console.log(ArrayportraitFIP[compteurAffichage])
   document.getElementById("petitNom").innerHTML = arrayName[compteurAffichage]
   let photo = document.getElementById('photo')
   photo.setAttribute('src', `${arrayPicture[compteurAffichage]}`)
   document.getElementById("adressAndDesc").innerHTML = ArrayportraitFIP[compteurAffichage]
+
   compteurAffichage+=1
   if(compteurAffichage>=ArrayportraitFIP.length){
     compteurAffichage = 0
@@ -65,18 +82,21 @@ function afficher(){
     compteurAffichage = 0
   }
 }
-  var t = 3000; 
-  setInterval(afficher,t)
 
+var t = 3000; 
+setInterval(afficher,t)
 
-
-
-
-//Marie Vassilieff 
-//Hélène Darroze//
-//Clara Malraux//
-//Yvonne Choquet- Bruhat//
-//Julia Sedefdjian//
-//Françoise Barré-Sinoussi//
-//Françoise Sagan//
-//Rosa Bonheur//
+const markers = arrayCoordonneesGPS.map((location, i) => {
+  console.log(location)
+  return new google.maps.Marker({
+    position: location,
+    label: labels[i % labels.length],
+    map:map,
+  });
+});
+//Add a marker clusterer to manage the markers.
+new MarkerClusterer(map, markers, {
+  imagePath:
+    "https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m",
+});
+//  imagePath: `${path}/m.png`})
